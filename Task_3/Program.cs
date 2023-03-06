737eb353916d58e81d5fe0b4959681ba5cbd78ea
Александр Шушпанов <aleksandrshushpanov@MacBook-Air-Aleksandr.local>
2023-03-06 11:02:02 +0300

    Task_3

---
+++ Task_3/Program.cs
@@ -0,0 +1,71 @@
+﻿//Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+using System;
+using static System.Console;
+Console.Clear();
+Write("Введите количество строк массива: ");
+int rows = int.Parse(ReadLine()!);
+Write("Введите количество столбцов массива: ");
+int column = int.Parse(ReadLine()!);
+int[,] mass = GetArray(rows, column, -10, 10);
+double sred = 0;
+FillArray(mass);
+PrintArray(mass);
+GetSred(mass);
+
+void FillArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            inArray[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Write($"{inArray[i, j]} ");
+        }
+        WriteLine();
+    }
+}
+void GetSred(int[,] array)
+{
+    for (int j = 0; j < mass.Length; j++)
+    {   int sum = 1;
+        for (int i = 0; i < mass.Length; i++)
+        {
+            if (j == i)
+            {
+                sum = sum + array[i, j];
+            }
+                }
+        sred = sum / rows;
+        Console.WriteLine(sred);
+        }
+
+    }
+
